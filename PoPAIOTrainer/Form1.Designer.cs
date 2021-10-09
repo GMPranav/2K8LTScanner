09cce0e268d794a32b6d828a7858eb4885c40659
GMP <pranavbtech@gmail.com>
2021-10-09 14:12:14 +0530

    Add Project Files.

---
+++ PoPAIOTrainer/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿
+namespace PoPAIOTrainer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pidLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Detected: ";
+            // 
+            // gameLabel
+            // 
+            this.gameLabel.AutoSize = true;
+            this.gameLabel.Location = new System.Drawing.Point(109, 37);
+            this.gameLabel.Name = "gameLabel";
+            this.gameLabel.Size = new System.Drawing.Size(23, 13);
+            this.gameLabel.TabIndex = 1;
+            this.gameLabel.Text = "NO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Process ID:";
+            // 
+            // pidLabel
+            // 
+            this.pidLabel.AutoSize = true;
+            this.pidLabel.Location = new System.Drawing.Point(109, 59);
+            this.pidLabel.Name = "pidLabel";
+            this.pidLabel.Size = new System.Drawing.Size(13, 13);
+            this.pidLabel.TabIndex = 3;
+            this.pidLabel.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PoP:2008 - LT Scanner";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "X Range";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Y Range";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Z Range";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Step";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(67, 104);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 17;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            950,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDownChartMinx_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(119, 104);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1100,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown2.TabIndex = 18;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1100,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDownChartMaxx_ValueChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(79, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Min";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(129, 88);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Max";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(119, 128);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            900,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown3.TabIndex = 22;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            850,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDownChartMaxy_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(67, 128);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            900,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown4.TabIndex = 21;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            450,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDownChartMiny_ValueChanged);
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(119, 154);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown5.TabIndex = 24;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown5.ValueChanged += new System.EventHandler(this.numericUpDownChartMaxz_ValueChanged);
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(67, 154);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown6.TabIndex = 23;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown6.ValueChanged += new System.EventHandler(this.numericUpDownChartMinz_ValueChanged);
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(67, 180);
+            this.numericUpDown7.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown7.TabIndex = 25;
+            this.numericUpDown7.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 253);
+            this.Controls.Add(this.numericUpDown7);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pidLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gameLabel);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "LT Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label gameLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label pidLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+    }
+}
+
---
+++ PoPAIOTrainer/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Memory;
+
+namespace PoPAIOTrainer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public Mem m = new Mem();
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void numericUpDownChartMinx_ValueChanged(object sender, EventArgs e)
+		{
+			numericUpDown2.Minimum = numericUpDown1.Value + 1;
+		}
+
+		private void numericUpDownChartMaxx_ValueChanged(object sender, EventArgs e)
+		{
+			numericUpDown1.Maximum = numericUpDown2.Value - 1;
+		}
+
+		private void numericUpDownChartMiny_ValueChanged(object sender, EventArgs e)
+		{
+			numericUpDown3.Minimum = numericUpDown4.Value + 1;
+		}
+
+		private void numericUpDownChartMaxy_ValueChanged(object sender, EventArgs e)
+		{
+			numericUpDown4.Maximum = numericUpDown3.Value - 1;
+		}
+
+		private void numericUpDownChartMinz_ValueChanged(object sender, EventArgs e)
+		{
+			numericUpDown5.Minimum = numericUpDown6.Value + 1;
+		}
+
+		private void numericUpDownChartMaxz_ValueChanged(object sender, EventArgs e)
+		{
+			numericUpDown6.Maximum = numericUpDown5.Value - 1;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+        {
+			button1.Invoke((MethodInvoker)delegate
+			{
+				button1.Text = "Scanning...";
+				button1.Enabled = false;
+			});
+
+			Thread.Sleep(100);
+			int pid = 0;
+			bool openproc = false;
+
+			if (m.GetProcIdFromName("PrinceOfPersia_Launcher") > 0)
+			{
+				pid = m.GetProcIdFromName("PrinceOfPersia_Launcher");
+				openproc = m.OpenProcess(pid);
+				gameLabel.Invoke((MethodInvoker)delegate
+				{
+					gameLabel.Text = "YES";
+				});
+				pidLabel.Invoke((MethodInvoker)delegate
+				{
+					pidLabel.Text = pid.ToString();
+				});
+
+				int ymin = Convert.ToInt32(numericUpDown4.Value);
+				int ymax = Convert.ToInt32(numericUpDown3.Value);
+				int zmin = Convert.ToInt32(numericUpDown6.Value);
+				int zmax = Convert.ToInt32(numericUpDown5.Value);
+				int xmin = Convert.ToInt32(numericUpDown1.Value);
+				int xmax = Convert.ToInt32(numericUpDown2.Value);
+				int step = Convert.ToInt32(numericUpDown7.Value);
+
+
+				for (int y = ymax; y >= ymin; y-=step)	//y is swept in reverse intentionally
+				{
+					for (int z = zmin; z <= zmax; z+=step)
+					{
+						for (int x = xmin; x <= xmax; x+=step)
+						{
+							int f = m.ReadInt("base+0xA44FC4");
+							m.WriteMemory("base+0x00A45B04,0x7BC,0x40,0x0,0xB8,0x30", "float", x.ToString());
+							m.WriteMemory("base+0x00A45B04,0x7BC,0x40,0x0,0xB8,0x34", "float", y.ToString());
+							m.WriteMemory("base+0x00A45B04,0x7BC,0x40,0x0,0xB8,0x38", "float", z.ToString());
+							while (f == m.ReadInt("base+0xA44FC4")) { continue; };	//Do not advance till the next frame
+						}
+					}
+				}
+
+			}
+			else
+			{
+				gameLabel.Invoke((MethodInvoker)delegate
+				{
+					gameLabel.Text = "NO";
+				});
+				pidLabel.Invoke((MethodInvoker)delegate
+				{
+					pidLabel.Text = "?";
+				});
+			}
+			button1.Invoke((MethodInvoker)delegate
+			{
+				button1.Enabled = true;
+				button1.Text = "Start";
+			});
+		}
+    }
+}

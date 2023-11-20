a1d215062326e122346016b2e9538734bb84941e
Pranav G <pranavbtech@gmail.com>
2023-11-20 13:25:29 +0530

    Max, min changes

--- PoPAIOTrainer/Form1.Designer.cs
+++ PoPAIOTrainer/Form1.Designer.cs
@@ -61,36 +61,40 @@ namespace PoPAIOTrainer
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 37);
+            this.label1.Location = new System.Drawing.Point(20, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.Size = new System.Drawing.Size(108, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Game Detected: ";
             // 
             // gameLabel
             // 
             this.gameLabel.AutoSize = true;
-            this.gameLabel.Location = new System.Drawing.Point(109, 37);
+            this.gameLabel.Location = new System.Drawing.Point(145, 46);
+            this.gameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.gameLabel.Name = "gameLabel";
-            this.gameLabel.Size = new System.Drawing.Size(23, 13);
+            this.gameLabel.Size = new System.Drawing.Size(27, 16);
             this.gameLabel.TabIndex = 1;
             this.gameLabel.Text = "NO";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 59);
+            this.label3.Location = new System.Drawing.Point(20, 73);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.Size = new System.Drawing.Size(76, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Process ID:";
             // 
             // pidLabel
             // 
             this.pidLabel.AutoSize = true;
-            this.pidLabel.Location = new System.Drawing.Point(109, 59);
+            this.pidLabel.Location = new System.Drawing.Point(145, 73);
+            this.pidLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.pidLabel.Name = "pidLabel";
-            this.pidLabel.Size = new System.Drawing.Size(13, 13);
+            this.pidLabel.Size = new System.Drawing.Size(14, 16);
             this.pidLabel.TabIndex = 3;
             this.pidLabel.Text = "?";
             // 
@@ -98,53 +102,59 @@ namespace PoPAIOTrainer
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(37, 9);
+            this.label2.Location = new System.Drawing.Point(49, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.Size = new System.Drawing.Size(157, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "PoP:2008 - LT Scanner";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 104);
+            this.label4.Location = new System.Drawing.Point(16, 128);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.Size = new System.Drawing.Size(59, 16);
             this.label4.TabIndex = 5;
             this.label4.Text = "X Range";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 130);
+            this.label5.Location = new System.Drawing.Point(16, 160);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.Size = new System.Drawing.Size(60, 16);
             this.label5.TabIndex = 8;
             this.label5.Text = "Y Range";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 156);
+            this.label6.Location = new System.Drawing.Point(16, 192);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.Size = new System.Drawing.Size(59, 16);
             this.label6.TabIndex = 11;
             this.label6.Text = "Z Range";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 182);
+            this.label7.Location = new System.Drawing.Point(16, 224);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.Size = new System.Drawing.Size(35, 16);
             this.label7.TabIndex = 14;
             this.label7.Text = "Step";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(57, 218);
+            this.button1.Location = new System.Drawing.Point(76, 268);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 16;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -152,19 +162,20 @@ namespace PoPAIOTrainer
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(67, 104);
+            this.numericUpDown1.Location = new System.Drawing.Point(89, 128);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Maximum = new decimal(new int[] {
-            1100,
+            2000,
             0,
             0,
             0});
             this.numericUpDown1.Minimum = new decimal(new int[] {
-            1000,
+            2000,
             0,
             0,
             -2147483648});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(61, 22);
             this.numericUpDown1.TabIndex = 17;
             this.numericUpDown1.Value = new decimal(new int[] {
             950,
@@ -175,19 +186,20 @@ namespace PoPAIOTrainer
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(119, 104);
+            this.numericUpDown2.Location = new System.Drawing.Point(159, 128);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown2.Maximum = new decimal(new int[] {
-            1100,
+            2000,
             0,
             0,
             0});
             this.numericUpDown2.Minimum = new decimal(new int[] {
-            1000,
+            2000,
             0,
             0,
             -2147483648});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(61, 22);
             this.numericUpDown2.TabIndex = 18;
             this.numericUpDown2.Value = new decimal(new int[] {
             1100,
@@ -199,36 +211,39 @@ namespace PoPAIOTrainer
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(79, 88);
+            this.label8.Location = new System.Drawing.Point(105, 108);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.Size = new System.Drawing.Size(28, 16);
             this.label8.TabIndex = 19;
             this.label8.Text = "Min";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(129, 88);
+            this.label9.Location = new System.Drawing.Point(172, 108);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.Size = new System.Drawing.Size(32, 16);
             this.label9.TabIndex = 20;
             this.label9.Text = "Max";
             // 
             // numericUpDown3
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(119, 128);
+            this.numericUpDown3.Location = new System.Drawing.Point(159, 158);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown3.Maximum = new decimal(new int[] {
-            900,
+            2000,
             0,
             0,
             0});
             this.numericUpDown3.Minimum = new decimal(new int[] {
-            500,
+            2000,
             0,
             0,
             -2147483648});
             this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown3.Size = new System.Drawing.Size(61, 22);
             this.numericUpDown3.TabIndex = 22;
             this.numericUpDown3.Value = new decimal(new int[] {
             850,
@@ -239,19 +254,20 @@ namespace PoPAIOTrainer
             // 
             // numericUpDown4
             // 
-            this.numericUpDown4.Location = new System.Drawing.Point(67, 128);
+            this.numericUpDown4.Location = new System.Drawing.Point(89, 158);
+            this.numericUpDown4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown4.Maximum = new decimal(new int[] {
-            900,
+            2000,
             0,
             0,
             0});
             this.numericUpDown4.Minimum = new decimal(new int[] {
-            500,
+            2000,
             0,
             0,
             -2147483648});
             this.numericUpDown4.Name = "numericUpDown4";
-            this.numericUpDown4.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown4.Size = new System.Drawing.Size(61, 22);
             this.numericUpDown4.TabIndex = 21;
             this.numericUpDown4.Value = new decimal(new int[] {
             450,
@@ -262,19 +278,20 @@ namespace PoPAIOTrainer
             // 
             // numericUpDown5
             // 
-            this.numericUpDown5.Location = new System.Drawing.Point(119, 154);
+            this.numericUpDown5.Location = new System.Drawing.Point(159, 190);
+            this.numericUpDown5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown5.Maximum = new decimal(new int[] {
-            300,
+            1000,
             0,
             0,
             0});
             this.numericUpDown5.Minimum = new decimal(new int[] {
-            500,
+            1000,
             0,
             0,
             -2147483648});
             this.numericUpDown5.Name = "numericUpDown5";
-            this.numericUpDown5.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown5.Size = new System.Drawing.Size(61, 22);
             this.numericUpDown5.TabIndex = 24;
             this.numericUpDown5.Value = new decimal(new int[] {
             300,
@@ -285,19 +302,20 @@ namespace PoPAIOTrainer
             // 
             // numericUpDown6
             // 
-            this.numericUpDown6.Location = new System.Drawing.Point(67, 154);
+            this.numericUpDown6.Location = new System.Drawing.Point(89, 190);
+            this.numericUpDown6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown6.Maximum = new decimal(new int[] {
-            300,
+            1000,
             0,
             0,
             0});
             this.numericUpDown6.Minimum = new decimal(new int[] {
-            500,
+            1000,
             0,
             0,
             -2147483648});
             this.numericUpDown6.Name = "numericUpDown6";
-            this.numericUpDown6.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown6.Size = new System.Drawing.Size(61, 22);
             this.numericUpDown6.TabIndex = 23;
             this.numericUpDown6.Value = new decimal(new int[] {
             100,
@@ -308,7 +326,8 @@ namespace PoPAIOTrainer
             // 
             // numericUpDown7
             // 
-            this.numericUpDown7.Location = new System.Drawing.Point(67, 180);
+            this.numericUpDown7.Location = new System.Drawing.Point(89, 222);
+            this.numericUpDown7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown7.Maximum = new decimal(new int[] {
             50,
             0,
@@ -320,7 +339,7 @@ namespace PoPAIOTrainer
             0,
             0});
             this.numericUpDown7.Name = "numericUpDown7";
-            this.numericUpDown7.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown7.Size = new System.Drawing.Size(61, 22);
             this.numericUpDown7.TabIndex = 25;
             this.numericUpDown7.Value = new decimal(new int[] {
             1,
@@ -330,9 +349,9 @@ namespace PoPAIOTrainer
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(193, 253);
+            this.ClientSize = new System.Drawing.Size(257, 311);
             this.Controls.Add(this.numericUpDown7);
             this.Controls.Add(this.numericUpDown5);
             this.Controls.Add(this.numericUpDown6);
@@ -355,6 +374,7 @@ namespace PoPAIOTrainer
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "LT Scanner";
